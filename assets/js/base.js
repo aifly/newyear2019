7a0ecee2bab3baff39092bef9e86ca454a8e1b1e
= <aifly_xc@163.com>
2019-01-27 22:50:18 +0800

    a

--- assets/js/base.js
+++ assets/js/base.js
@@ -51,8 +51,8 @@ for (var attr in imgs) {
 
 
 window.config = {
-  indexvideo:'./assets/video/index2.mp4',
-  indexvideo1:'./assets/video/index1.mp4',
+  indexvideo:'./assets/video/index3.mp4',
+  indexvideo1:'./assets/video/index3.mp4',
   baseUrl: "https://xlive.xinhuaapp.com",
   secretKey: "e9469538b0623783f38c585821459454",
   wishes:[
